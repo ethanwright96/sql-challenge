6657c29cc7972142e75e827c7142a9c08637b7f1
Ethan Wright <ethan_wright@alumni.brown.edu>
2023-05-04 12:32:49 -0400

    began analysis portion

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -77,12 +77,11 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-SELECT * FROM departments
 
 -- Tables:
-DROP TABLE departments;
-DROP TABLE dept_emp;
-DROP TABLE dept_manager;
-DROP TABLE employees;
-DROP TABLE salaries;
-DROP TABLE titles;
+-- DROP TABLE departments;
+-- DROP TABLE dept_emp;
+-- DROP TABLE dept_manager;
+-- DROP TABLE employees;
+-- DROP TABLE salaries;
+-- DROP TABLE titles;
